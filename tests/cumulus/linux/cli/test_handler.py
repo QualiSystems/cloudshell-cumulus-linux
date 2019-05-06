e9a0f1c79afa9d57666182e4e326aa8987f7a05c
Anthony <anton.p@qualisystems.com>
2019-05-06 12:46:47 +0300

    add empty test

---
+++ tests/cumulus/linux/cli/test_handler.py
@@ -0,0 +1,21 @@
+import unittest
+
+import mock
+
+from cloudshell.cumulus.linux.cli.handler import CumulusCliHandler
+
+
+class TestCliHandlerImpl(unittest.TestCase):
+    def setUp(self):
+        """
+
+        :return:
+        """
+        self.cli = mock.MagicMock()
+        self.resource_config = mock.MagicMock()
+        self.logger = mock.MagicMock()
+        self.api = mock.MagicMock()
+        self.cli_handler = CumulusCliHandler(cli=self.cli,
+                                             resource_config=self.resource_config,
+                                             api=self.api,
+                                             logger=self.logger)
