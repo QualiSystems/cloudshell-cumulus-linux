8dd1c3b5387d31d59d7407c2820556d0467cdf43
Kyrylo Maksymenko <saklar13@gmail.com>
2022-01-18 16:54:34 +0200

    update CLI Handler

--- cloudshell/cumulus/linux/cli/handler.py
+++ cloudshell/cumulus/linux/cli/handler.py
@@ -1,23 +1,44 @@
-from cloudshell.cli.command_mode_helper import CommandModeHelper
-from cloudshell.cumulus.linux.cli.command_modes import DefaultCommandMode
-from cloudshell.cumulus.linux.cli.command_modes import RootCommandMode
-from cloudshell.devices.cli_handler_impl import CliHandlerImpl
+from logging import Logger
+
+from cloudshell.cli.configurator import AbstractModeConfigurator
+from cloudshell.cli.service.cli import CLI
+from cloudshell.cli.service.command_mode_helper import CommandModeHelper
+from cloudshell.cli.service.session_pool_manager import SessionPoolManager
+from cloudshell.cli.session.ssh_session import SSHSession
+from cloudshell.cli.session.telnet_session import TelnetSession
+from cloudshell.shell.standards.networking.resource_config import (
+    NetworkingResourceConfig,
+)
+
+from cloudshell.cumulus.linux.cli.command_modes import (
+    DefaultCommandMode,
+    RootCommandMode,
+)
 
 
-class CumulusCliHandler(CliHandlerImpl):
-    def __init__(self, cli, resource_config, logger, api):
-        super(CumulusCliHandler, self).__init__(cli, resource_config, logger, api)
-        self.modes = CommandModeHelper.create_command_mode(resource_config, api)
+def get_cli(resource_config: NetworkingResourceConfig) -> CLI:
+    session_pool_size = int(resource_config.sessions_concurrency_limit)
+    session_pool = SessionPoolManager(max_pool_size=session_pool_size)
+    return CLI(session_pool=session_pool)
+
+
+class CumulusCliHandler(AbstractModeConfigurator):
+    REGISTERED_SESSIONS = (SSHSession, TelnetSession)
+
+    def __init__(
+        self, cli: CLI, resource_config: NetworkingResourceConfig, logger: Logger
+    ):
+        super().__init__(resource_config, logger, cli)
+        self.modes = CommandModeHelper.create_command_mode(resource_config)
 
     @property
-    def enable_mode(self):
+    def enable_mode(self) -> DefaultCommandMode:
         return self.modes[DefaultCommandMode]
 
     @property
-    def config_mode(self):
+    def config_mode(self) -> DefaultCommandMode:
         return self.modes[DefaultCommandMode]
 
     @property
-    def root_mode(self):
+    def root_mode(self) -> RootCommandMode:
         return self.modes[RootCommandMode]
-
