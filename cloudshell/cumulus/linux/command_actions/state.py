d665ffcd2777020648766714853d59a4c305e69b
Kyrylo Maksymenko <saklar13@gmail.com>
2022-01-18 18:00:21 +0200

    update shutdown command

--- cloudshell/cumulus/linux/command_actions/state.py
+++ cloudshell/cumulus/linux/command_actions/state.py
@@ -1,25 +1,28 @@
-from cloudshell.cli.command_template.command_template_executor import CommandTemplateExecutor
+from __future__ import annotations
+
+from logging import Logger
+
+import attr
+
+from cloudshell.cli.command_template.command_template_executor import (
+    CommandTemplateExecutor,
+)
+from cloudshell.cli.service.cli_service import CliService
+
 from cloudshell.cumulus.linux.command_templates import state
 
 
-class StateActions(object):
-    def __init__(self, cli_service, logger):
-        """
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class StateActions:
+    _cli_service: CliService
+    _logger: Logger
 
-        :param cli_service:
-        :param logger:
-        """
-        self._cli_service = cli_service
-        self._logger = logger
-
-    def shutdown(self, action_map=None, error_map=None):
-        """
-
-        :param action_map:
-        :param error_map:
-        :return:
-        """
-        return CommandTemplateExecutor(cli_service=self._cli_service,
-                                       command_template=state.SHUTDOWN,
-                                       action_map=action_map,
-                                       error_map=error_map).execute_command()
+    def shutdown(
+        self, action_map: dict | None = None, error_map: dict | None = None
+    ) -> str:
+        return CommandTemplateExecutor(
+            self._cli_service,
+            state.SHUTDOWN,
+            action_map=action_map,
+            error_map=error_map,
+        ).execute_command()
