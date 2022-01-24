82542c3dc2bc2d69f6cd43f1454587259c1d08a7
Kyrylo Maksymenko <saklar13@gmail.com>
2022-01-24 15:24:29 +0200

    load firmware command

--- cloudshell/cumulus/linux/command_actions/firmware.py
+++ cloudshell/cumulus/linux/command_actions/firmware.py
@@ -1,28 +1,21 @@
-from cloudshell.cli.command_template.command_template_executor import CommandTemplateExecutor
+from logging import Logger
+
+import attr
+
+from cloudshell.cli.command_template.command_template_executor import (
+    CommandTemplateExecutor,
+)
+from cloudshell.cli.service.cli_service import CliService
+
 from cloudshell.cumulus.linux.command_templates import firmware
 
 
-class FirmwareActions(object):
-    def __init__(self, cli_service, logger):
-        """
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class FirmwareActions:
+    _cli_service: CliService
+    _logger: Logger
 
-        :param cli_service:
-        :param logger:
-        """
-        self._cli_service = cli_service
-        self._logger = logger
-
-    def load_firmware(self, image_path, timeout, action_map=None, error_map=None):
-        """
-
-        :param image_path:
-        :param timeout:
-        :param action_map:
-        :param error_map:
-        :return:
-        """
-        return CommandTemplateExecutor(cli_service=self._cli_service,
-                                       command_template=firmware.LOAD_FIRMWARE,
-                                       action_map=action_map,
-                                       timeout=timeout,
-                                       error_map=error_map).execute_command(image_path=image_path)
+    def load_firmware(self, image_path: str, timeout) -> str:
+        return CommandTemplateExecutor(
+            self._cli_service, firmware.LOAD_FIRMWARE, timeout=timeout
+        ).execute_command(image_path=image_path)
