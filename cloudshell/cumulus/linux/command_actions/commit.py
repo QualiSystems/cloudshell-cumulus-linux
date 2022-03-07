789ff0376ada20dcf957ba4c05e128b47da59a3d
Kyrylo Maksymenko <saklar13@gmail.com>
2022-03-07 11:45:20 +0200

    formatting

--- cloudshell/cumulus/linux/command_actions/commit.py
+++ cloudshell/cumulus/linux/command_actions/commit.py
@@ -1,42 +1,29 @@
-from cloudshell.cli.command_template.command_template_executor import CommandTemplateExecutor
+from logging import Logger
+from typing import ClassVar
+
+import attr
+
+from cloudshell.cli.command_template.command_template_executor import (
+    CommandTemplateExecutor,
+)
+from cloudshell.cli.service.cli_service import CliService
+
 from cloudshell.cumulus.linux.command_templates import commit_abort
 
 
-class CommitActions(object):
-    COMMIT_COMMAND_TIMEOUT = 5 * 60
-    ABORT_COMMAND_TIMEOUT = 5 * 60
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class CommitActions:
+    COMMIT_COMMAND_TIMEOUT: ClassVar[int] = 5 * 60
+    ABORT_COMMAND_TIMEOUT: ClassVar[int] = 5 * 60
+    _cli_service: CliService
+    _logger: Logger
 
-    def __init__(self, cli_service, logger):
-        """
+    def commit(self) -> str:
+        return CommandTemplateExecutor(
+            self._cli_service, commit_abort.COMMIT, timeout=self.COMMIT_COMMAND_TIMEOUT
+        ).execute_command()
 
-        :param cli_service:
-        :param logger:
-        """
-        self._cli_service = cli_service
-        self._logger = logger
-
-    def commit(self, action_map=None, error_map=None):
-        """
-
-        :param action_map:
-        :param error_map:
-        :return:
-        """
-        return CommandTemplateExecutor(cli_service=self._cli_service,
-                                       command_template=commit_abort.COMMIT,
-                                       action_map=action_map,
-                                       timeout=self.COMMIT_COMMAND_TIMEOUT,
-                                       error_map=error_map).execute_command()
-
-    def abort(self, action_map=None, error_map=None):
-        """
-
-        :param action_map:
-        :param error_map:
-        :return:
-        """
-        return CommandTemplateExecutor(cli_service=self._cli_service,
-                                       command_template=commit_abort.ABORT,
-                                       timeout=self.ABORT_COMMAND_TIMEOUT,
-                                       action_map=action_map,
-                                       error_map=error_map).execute_command()
+    def abort(self) -> str:
+        return CommandTemplateExecutor(
+            self._cli_service, commit_abort.ABORT, timeout=self.ABORT_COMMAND_TIMEOUT
+        ).execute_command()
