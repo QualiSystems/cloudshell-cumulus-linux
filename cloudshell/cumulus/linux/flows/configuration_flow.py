bb1ac80df415f6c3c346ddb0fe8a44de1e07f437
Kyrylo Maksymenko <saklar13@gmail.com>
2022-05-12 16:45:46 +0300

    use new version of shell-flows

--- cloudshell/cumulus/linux/flows/configuration_flow.py
+++ cloudshell/cumulus/linux/flows/configuration_flow.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
-import os
 from logging import Logger
 
-from cloudshell.shell.flows.configuration.basic_flow import AbstractConfigurationFlow
+from cloudshell.shell.flows.configuration.basic_flow import (
+    AbstractConfigurationFlow,
+    ConfigurationType,
+    RestoreMethod,
+)
+from cloudshell.shell.flows.utils.url import LocalFileURL
 from cloudshell.shell.standards.networking.resource_config import (
     NetworkingResourceConfig,
 )
@@ -53,6 +57,10 @@ SERVICES_TO_RESTART = ("mstpd", "frr", "sshd", "lldpd", "switchd")
 
 
 class ConfigurationFlow(AbstractConfigurationFlow):
+    LOCAL_URL_CLASS = LocalFileURL
+    SUPPORTED_CONFIGURATION_TYPES = {ConfigurationType.RUNNING}
+    SUPPORTED_RESTORE_METHODS = {RestoreMethod.OVERRIDE}
+
     def __init__(
         self,
         logger: Logger,
@@ -63,12 +71,15 @@ class ConfigurationFlow(AbstractConfigurationFlow):
         self._cli_configurator = cli_configurator
 
     @property
-    def _file_system(self) -> str:
-        return "file"
+    def file_system(self) -> str:
+        return "file:/"
 
     def _save_flow(
-        self, folder_path: str, configuration_type: str, vrf_management_name: str | None
-    ) -> None:
+        self,
+        file_dst_url: ConfigurationFlow.REMOTE_URL_CLASS | LOCAL_URL_CLASS,
+        configuration_type: ConfigurationType,
+        vrf_management_name: str | None,
+    ) -> str | None:
         with self._cli_configurator.root_mode_service() as cli_service:
             sys_act = SystemActions(cli_service, self._logger)
 
@@ -86,16 +97,19 @@ class ConfigurationFlow(AbstractConfigurationFlow):
             sys_act.tar_compress_folder(compress_name=backup_file, folder=backup_dir)
 
             self._logger.info(f"Uploading backup .tar archive '{backup_file}' via curl")
-            if folder_path.startswith(self._local_path):
-                folder = self._get_folder_from_local_url(folder_path)
+            if isinstance(file_dst_url, LocalFileURL):
+                folder = file_dst_url.get_folder()
                 sys_act.create_folder(folder)
-            sys_act.curl_upload_file(file_path=backup_file, remote_url=folder_path)
+            sys_act.curl_upload_file(
+                file_path=backup_file, remote_url=str(file_dst_url)
+            )
+        return None
 
     def _restore_flow(
         self,
-        path: str,
-        configuration_type: str,
-        restore_method: str,
+        config_path: ConfigurationFlow.REMOTE_URL_CLASS | LOCAL_URL_CLASS,
+        configuration_type: ConfigurationType,
+        restore_method: RestoreMethod,
         vrf_management_name: str | None,
     ) -> None:
         with self._cli_configurator.root_mode_service() as cli_service:
@@ -103,7 +117,9 @@ class ConfigurationFlow(AbstractConfigurationFlow):
 
             self._logger.info("Downloading backup files")
             backup_file = sys_act.create_tmp_file()
-            sys_act.curl_download_file(remote_url=path, file_path=backup_file)
+            sys_act.curl_download_file(
+                remote_url=str(config_path), file_path=backup_file
+            )
 
             self._logger.info("Uncompressing backup files to the system")
             sys_act.tar_uncompress_folder(compressed_file=backup_file, destination="/")
@@ -114,21 +130,3 @@ class ConfigurationFlow(AbstractConfigurationFlow):
             for service in SERVICES_TO_RESTART:
                 self._logger.info(f"Restarting '{service}' service")
                 sys_act.restart_service(name=service)
-
-    def _get_path(self, path: str = "") -> str:
-        path = super()._get_path(path)
-        path = self._update_local_path(path)
-        return path
-
-    @property
-    def _local_path(self) -> str:
-        return f"{self._file_system}://localhost/"
-
-    def _update_local_path(self, path: str) -> str:
-        if path.startswith(self._file_system) and not path.startswith(self._local_path):
-            scheme_len = len(f"{self._file_system}://")
-            path = f"{self._local_path}{path[scheme_len:]}"
-        return path
-
-    def _get_folder_from_local_url(self, path: str) -> str:
-        return os.path.dirname(path[len(self._local_path) - 1 :])
