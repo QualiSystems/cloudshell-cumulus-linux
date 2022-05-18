b9efa6b79bab35a95c6a3a170feab99656efc0a8
Kyrylo Maksymenko <saklar13@gmail.com>
2022-05-18 15:43:47 +0300

    add specific Errors for Command Templates

--- cloudshell/cumulus/linux/command_templates/__init__.py
+++ cloudshell/cumulus/linux/command_templates/__init__.py
@@ -1,4 +1,17 @@
+from cloudshell.cli.session.session_exceptions import CommandExecutionException
+
+
+class CommandNotFound(CommandExecutionException):
+    def __init__(self):
+        super().__init__("Command not found")
+
+
+class CommandError(CommandExecutionException):
+    def __init__(self):
+        super().__init__("Command error")
+
+
 ERROR_MAP = {
-    r"[Cc]ommand not found": "Command not found",
-    r"[Ee]rror:|ERROR:": "Command error",
+    r"[Cc]ommand not found": CommandNotFound(),
+    r"[Ee]rror:|ERROR:": CommandError(),
 }
